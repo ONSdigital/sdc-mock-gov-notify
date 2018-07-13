39e85b503946682854566efdebaf4ce8307f3d74
Billie Thompson <billie.thompson@armakuni.co.uk>
2018-07-13 11:23:31 +0100

    Launch application for tests
    Currently we use the flask test client, this isn't a very good test
    because it doesn't allow us to use the real gov notify client. This will
    enable us to do that.

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,44 @@
+import os
+import subprocess
+from time import sleep
+
+import requests
+
 from sdc_mock_gov_notify import app
 
 
-def before_scenario(context, _step):
+def before_feature(context, _step):
+    port = os.getenv("PORT", 3001)
+    host = 'localhost'
+    command = f'pipenv run gunicorn -b {host}:{port} --workers 1 --timeout 60 sdc_mock_gov_notify:app'
+    context.app_url = f'http://{host}:{port}'
+
+    print(f'Starting server with command {command}')
     app.testing = True
-    context.app = app.test_client()
+    context.process = subprocess.Popen(command, shell=True)
+
+    _wait_for_server(f'{context.app_url}/inbox/emails', retries=10)
+
+
+def after_feature(context, _scenario):
+    context.process.terminate()
+
+
+def _wait_for_server(test_url, retries):
+    print('Waiting for server to start')
+    started = False
+    while (not started) and retries > 1:
+        try:
+            response = requests.get(test_url, timeout=30)
+            if response.status_code == requests.codes.ok:
+                started = True
+            else:
+                print('retrying')
+
+        except:
+            print(f'retrying...')
+            sleep(1)
+            retries -= 1
+    if not started:
+        raise Exception('Failed to start server')
+    print('Server started')
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,35 +1,35 @@
-import json
+from behave import *
+
+import requests
 from nose.tools import assert_equals
 
 
 @given(u'an email is sent to Gov Notify to "{email_address}"')
 def send_email_to_gov_notify(context, email_address):
 
-    response = context.app.post(
-        '/gov_notify_api/v2/notifications/email',
-        data=json.dumps({'email_address': email_address,
-                         'template_id': 'this-is-a-uuid',
-                         'personalisation': {'name': 'Tom'},
-                         'reference': 'example-reference'}),
-        content_type='application/json')
+    response = requests.post(
+        f'{context.app_url}/gov_notify_api/v2/notifications/email',
+        json={'email_address': email_address,
+              'template_id': 'this-is-a-uuid',
+              'personalisation': {'name': 'Tom'},
+              'reference': 'example-reference'})
 
     assert_equals(200, response.status_code)
 
 
 @when(u'I check the email inbox')
 def check_inbox(context):
-    response = context.app.get('/inbox/emails')
+    response = requests.get(f'{context.app_url}/inbox/emails')
 
     assert_equals(200, response.status_code)
 
-    context.emails = json.loads(response.data)
+    context.emails = response.json()
     print(context.emails)
 
 
 @when(u'I clear the email inbox')
 def clear_inbox(context):
-    response = context.app.delete('/inbox/emails')
-
+    response = requests.delete(f'{context.app_url}/inbox/emails')
     assert_equals(200, response.status_code)
 
 
